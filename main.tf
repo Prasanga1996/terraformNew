e6de3e425ce255be8da6a36191a18a49df54278e
Prasanga1996 <31898681+Prasanga1996@users.noreply.github.com>
2023-02-22 17:40:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,3 +16,10 @@ provider "azurerm" {
   client_secret   = "yWV8Q~eSdrVr6ZgWnj4r~dZdnScm5l10w.wSMcit"
 }
 
+ backend "azurerm" {
+    resource_group_name = var.bkstrgrg
+    storage_account_name = var.bkstrg
+    container_name = var.bkcontainer
+    key = var.bkstrgkey
+  }
+}
